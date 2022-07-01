52b45d814149cf14a05fb39a1add5a896439b6c9
Moore-K2 <1104326586@qq.com>
2022-07-01 21:18:18 +0800

    验证码的实现

--- src/views/order/Index.vue
+++ src/views/order/Index.vue
@@ -1,12 +1,38 @@
 <template>
   <div>
-    <h1 style="padding-top: 40px">我是订单</h1>
+    <div class="container">
+      <el-button type="success" plain @click="displayCode">随机码</el-button>
+      <h1>{{ result }}</h1>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Index",
+  data() {
+    return {
+      str: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+      result: "",
+    };
+  },
+  methods: {
+    displayCode() {
+      const arr = this.str.split("");
+      for (var i = 0; i < 6; i++) {
+        // n 在(0, arr.length)范围内
+        var n = Math.floor(Math.random() * arr.length);
+        this.result += arr[n];
+      }
+      return this.result;
+    },
+  },
+};
 </script>
 
 <style>
+.container {
+  margin-top: 40px;
+  margin-left: 40px;
+}
 </style>
